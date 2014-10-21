b449e8ce94cc8145d6e8a6f13f46b314dc826750
Taylor Lapeyre <taylorlapeyre@gmail.com>
2014-10-21 13:04:56 -0500

    add a note

--- src-cljs/myapp/hello.cljs
+++ src-cljs/myapp/hello.cljs
@@ -1,6 +1,9 @@
 (ns myapp.hello
   (:require [reagent.core :as reagent :refer [atom]]))
 
+; NOTE: This doesn't do anything yet, but it would work if you ran
+; `myapp.hello.run() in the JS console.`
+
 (defn greeting [message]
   [:h1 message])
 
